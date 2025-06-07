e1e4cf96ea4e5950f9034a43d7a8333467c78ebd
lijiaren <lijiaren1990@gmail.com>
2025-06-07 16:28:25 +0800

    初始化

---
+++ familyGoodsCenter/src/main/java/com/ren/goods/FamilyGoodsCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ren.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyGoodsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyGoodsCenterApplication.class, args);
+	}
+
+}
---
+++ familyGoodsCenter/src/test/java/com/ren/goods/FamilyGoodsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ren.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyGoodsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
